40cd4b3fd4f398d168d2dfeb0521c65a2df4d318
Studio Scholz <dev@studioscholz.info>
2018-06-09 14:16:56 +0200

    New components

---
+++ components/InformationClients.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="information__clients">
+    <h2>Clients</h2>
+    <ol>
+      <li class="information__clients__entry" v-for="client in clients">{{client}}</li>
+    </ol>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'InformationClients',
+  props: ['clients'],
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="sass">
+@import "../assets/sass/variables.sass"
+
+.information
+  @include fs-m()
+  &__clients
+    padding-bottom: $lh-m
+    h2
+    &__entry
+      list-style: none
+</style>
---
+++ components/LegalNotice.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="legalnotice">
+    <h4 class="legalnotice__headline">Legal Notice</h4>
+    <span>{{title}}</span>
+    <p v-html="address"></p>
+    <span>{{phone}} {{email}}</span>
+    <p class="legalnotice__text" v-html="legalnotice"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LegalNotice',
+  props: ['information'],
+  computed: {
+    title() {
+      return this.information.title
+    },
+    about() {
+      return this.information.about
+    },
+    street() {
+      return this.information.street
+    },
+    postcode() {
+      return this.information.postcode
+    },
+    city() {
+      return this.information.city
+    },
+    country() {
+      return this.information.country
+    },
+    phone() {
+      return this.information.phone
+    },
+    email() {
+      return this.information.email
+    },
+    legalnotice() {
+      return this.information.legalnotice
+    },
+    address() {
+      let fullAddress = '<span>' + this.street + '</span>'
+      fullAddress += this.postcode.length > 0 && this.city.length > 0 ? '<span class="nobr"> ' + this.postcode + ' ' + this.city  : ''
+      fullAddress += this.country.length > 0 ? ' (' + this.country + ')</span>' : '</span>'
+
+      return fullAddress
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  @import "../assets/sass/variables.sass"
+
+  .legalnotice
+    max-width: 550px
+    padding: $mp-e 0 0 0
+    @include fs-xs()
+    &__text
+      margin-top: $lh-xs
+
+</style>
